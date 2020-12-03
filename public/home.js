4d0be54379658414121b39b67ac40d9cb586896e
aliengamer38 <aliengamer38@gmail.com>
2020-12-03 18:50:55 +0800

    updated commit

--- public/home.js
+++ public/home.js
@@ -1,3 +1,12 @@
-// $("#run").on("click", function (e) {
-    
-// })
+
+
+$(function () {
+    $("#run").on("click", function (e) {
+        const val = $("p").map(function (index, element) {
+            return $(element).text();
+        });
+        console.log(val.get());
+    })
+});
+
+
