de484033bcdccf627ba909313900b596120a795b
aliengamer38 <aliengamer38@gmail.com>
2020-12-09 14:07:56 +0800

    add

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -1,14 +1,12 @@
-$(".box").animate({
-    height: "15rem"
-}, 2000)
-$(".square").animate({
-    height: "15rem"
-}, 2000)
-const init = function () {
-    let sum = 0;
-    for (let i = 0; i < Math.pow(10, 10); i++) {
-        sum =+ i;
-    }
-    console.log(sum);
-};
-init();
+$(function () {
+    $("#name").val("");
+    $("#btn").on("click", function (e) {
+        // $(".container").load("/process", {text: "text"});
+    })
+    $("#name").on("keyup", function (e) {
+        $.get("/process", {input:$(this).val()}, function (res) {        
+            let result = res.result;            
+            $(".container").html(`<p>${Math.sin(result)}</p>`);
+        })
+    })
+})
