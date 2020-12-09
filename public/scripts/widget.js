8eca502318f2cdcc1bfc7b4512cd38c1f69058d9
aliengamer38 <aliengamer38@gmail.com>
2020-12-09 14:01:09 +0800

    new

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -1,14 +1,39 @@
-$(".box").animate({
-    height: "15rem"
-}, 2000)
-$(".square").animate({
-    height: "15rem"
-}, 2000)
-const init = function () {
-    let sum = 0;
-    for (let i = 0; i < Math.pow(10, 10); i++) {
-        sum =+ i;
-    }
-    console.log(sum);
-};
-init();
+
+// $(".box")
+//     .animate({
+//         height: "15rem"
+//     }, 1000)
+//     .queue(function () {
+//         console.log($(this).queue("fx"));
+//         const box = $(this);
+//         setTimeout(function () {
+//             box.dequeue();    
+//             console.log(box.queue("fx"));
+//         }, 1000)
+//     })
+//     .animate({
+//         fontSize: "4rem"
+//     }, 1000, function () {
+//             console.log("done");
+//     })
+// $(".square")
+//     .animate({
+//         height: "15rem"
+//     }, 1000)
+//     .animate({
+//         fontSize: "4rem"
+//     }, 1000);
+
+// // setTimeout(function () {
+// //     console.log("done!");
+// // }, 1000)
+// // $(".timeout").animate({
+// //     "font-size": "2rem"
+// // }, 3000)
+// $.fx.off = true;
+$({ counter: 1, index: 0 }).animate({
+    counter: 100, index: 99
+}, {
+    duration: 1000, step: function (now, tween) { 
+        console.log("test");
+}})
